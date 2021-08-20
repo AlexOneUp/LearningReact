47419f839fafb65e46136397c6776b8882484373
AlexOneUp <ruanalex11235@gmail.com>
2021-08-19 23:20:31 -0400

    Learning JSX

---
+++ Section 2: JSX/index.js
@@ -0,0 +1,15 @@
+class JSXDemo extends React.Component {
+    render() {
+        return (
+            <div>
+
+                <h1>
+                    My Image
+            <img src='https://images.unsplash.com/photo-1494500764479-0c8f2919a3d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80'></img>
+                </h1>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<JSXDemo />, document.getElementById('root'))
