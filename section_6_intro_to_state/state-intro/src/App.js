770f8d09f93a9b822068bc4490736f26f91fe01d
AlexOneUp <ruanalex11235@gmail.com>
2021-08-22 17:15:00 -0400

    Section 6 Intro to State: learning to use setState() to update state

--- section_6_intro_to_state/state-intro/src/App.js
+++ section_6_intro_to_state/state-intro/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Game from './Game';
 import Demo from './Demo';
+import Rando from './Rando';
 
 function App() {
   return (
     <div className="App">
-      <Game color="purple"/>
-      <Demo animal="bobcat" food="pineapple"/>
+      <Rando maxNum={7}/>
+
     </div>
   );
 }
--- section_6_intro_to_state/state-intro/src/Game.js
+++ section_6_intro_to_state/state-intro/src/Game.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 
 class Game extends Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         score: 0,
-    //         gameOver: false
-    //     }
-    // }
-
-    // Alternative syntax using Babel which converts this code
-    state = {
-        score: 0,
-        gameOver: false
+    constructor(props) {
+        super(props);
+        this.state = {
+            score: 99,
+            gameOver: false
+        }
     }
 
-    render(){
-        return(
+    // Alternative syntax using Babel which converts this code
+    // state = {
+    //     score: 0,
+    //     gameOver: false
+    // }
+
+    render() {
+        return (
             <div>
                 <h1>Your Score is : {this.state.score}</h1>
             </div>
