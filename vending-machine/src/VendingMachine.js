9252aedf891562045b5f7c1f0bac42c3a92b0164
AlexOneUp <ruanalex11235@gmail.com>
2021-08-28 00:57:38 -0400

    Final Project: Color Palette Application

--- vending-machine/src/VendingMachine.js
+++ vending-machine/src/VendingMachine.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import vendingMachineImg from './images/vendingmachine.png';
 import { NavLink } from 'react-router-dom';
+import Message from './components/Message';
 
 
 export class VendingMachine extends Component {
@@ -8,8 +9,10 @@ export class VendingMachine extends Component {
         return (
             <div
                 className='VendingMachine'
-                style={{ backgroundImage: `url(${vendingMachineImg})` }}
+                style={{ backgroundImage: `url(https://media2.giphy.com/media/3oKIPaI8ZrQS9wnigM/giphy.gif?cid=ecf05e471gq0ajechjfew54nbft5ai0vkxpbqydroe35jigc&rid=giphy.gif&ct=g)` }}
             >
+            <Message/>
+            
                 <h1>Hello, I am a vending machine</h1>
                 <NavLink to="/soda">Soda</NavLink>
                 <NavLink to="/chips">Chips</NavLink>
